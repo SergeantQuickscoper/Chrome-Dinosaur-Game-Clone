0acc1519d8ad01d384a8e70ea8ca8be4ec8578ff
Don Roy Chacko <donisepic30@gmail.com>
2020-07-23 17:40:40 +0300

    Bug Fixes and Basic Level Generator created

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField]private Transform levelPart;
+    [SerializeField]private Transform startingLevelPart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform lastLevelPart = SpawnLevelPart(startingLevelPart.Find("EndPosition").position);
+        lastLevelPart = SpawnLevelPart(lastLevelPart.Find("EndPosition").position);
+        lastLevelPart = SpawnLevelPart(lastLevelPart.Find("EndPosition").position)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform SpawnLevelPart(Vector3 SpawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart, SpawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
+}
