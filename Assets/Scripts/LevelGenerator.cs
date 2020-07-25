be75b7abf70c0aafa2b3cef2db67db490ceb13e1
Don Roy Chacko <donisepic30@gmail.com>
2020-07-25 17:47:38 +0300

    Completed Level Generation Script

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
 
-    [SerializeField]private Transform levelPart;
+    private const float PLAYER_DISTANCE_TO_SPAWN_LEVEL_PART = 50f;
+    [SerializeField]private Transform[] levelParts;
     [SerializeField]private Transform startingLevelPart;
+    [SerializeField]private Transform PlayerReference;
+    private Vector3 lastEndPosition;
+    private float nextTimeToSearch = 0;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        Transform lastLevelPart = SpawnLevelPart(startingLevelPart.Find("EndPosition").position);
-        lastLevelPart = SpawnLevelPart(lastLevelPart.Find("EndPosition").position);
-        lastLevelPart = SpawnLevelPart(lastLevelPart.Find("EndPosition").position)
+        lastEndPosition = startingLevelPart.Find("EndPosition").position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerReference == null) {
+			FindPlayer ();
+			return;
+		}
+
+        if(Vector3.Distance(PlayerReference.position, lastEndPosition) < PLAYER_DISTANCE_TO_SPAWN_LEVEL_PART)
+        {
+            SpawnLevelPart();
+        }
     }
 
-    public Transform SpawnLevelPart(Vector3 SpawnPosition)
+    public void SpawnLevelPart()
     {
-        Transform levelPartTransform = Instantiate(levelPart, SpawnPosition, Quaternion.identity);
+        Transform chosenLevelPart = levelParts[Random.Range(0, levelParts.Length)];
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition, chosenLevelPart);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    public Transform SpawnLevelPart(Vector3 SpawnPosition, Transform LevelPart)
+    {
+        Transform levelPartTransform = Instantiate(LevelPart, SpawnPosition, Quaternion.identity);
         return levelPartTransform;
     }
+
+    void FindPlayer () {
+		if (nextTimeToSearch <= Time.time) {
+			GameObject searchResult = GameObject.FindGameObjectWithTag ("Player");
+			if (searchResult != null)
+				PlayerReference = searchResult.transform;
+			nextTimeToSearch = Time.time + 0.5f;
+		}
+	}
 }
